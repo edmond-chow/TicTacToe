ce17676210292780f4d6ea2ad7b8b4f2921e9707
Edmond Chow <45330596+edmond-chow@users.noreply.github.com>
2025-04-07 14:07:34 +0800

    Add files via upload

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,8 +4,8 @@
  *   A game you can be an Attacker or Defender, as a user you may put an O
  *   chess while the program might response from a X chess. Which of the
  *   roles also gives you a chance to simulate within various cases in Debug
- *   mode. The code enumerates a course of options, the modes is encoded in
- *   the 2-bit field from 32-bit type Board, and the one exceed 2-bit is
+ *   mode. The code enumerates a course of options, the modes are encoded in
+ *   the 2-bit field from a 32-bit type Board, and the one exceeding 2-bit is
  *   treated as a control code to NewGame. The Startup code intends to just
  *   reset the game without switching into other encoded mode. The Conjugate
  *   code switches in between Attacker or Defender while the Configurate code
